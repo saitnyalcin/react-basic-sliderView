6708fb82496d75c816f869adc4c4c02083085c35
steveyalcin <steven.yalcin@gmail.com>
2020-01-19 17:11:58 -0400

    first commit

---
+++ src/components/SliderView.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Slide } from 'react-slideshow-image';
+import CitadelHill from '../images/citadelHill.jpg';
+import HalifaxTown from '../images/halifaxTown.jpg';
+import LightHouse from '../images/lightHouse.jpg';
+import './SliderView.css';
+
+const slideImages = [CitadelHill, HalifaxTown, LightHouse];
+
+const properties = {
+  duration: 5000,
+  transitionDuration: 500,
+  infinite: true,
+  indicators: true,
+  arrows: true,
+  onChange: (oldIndex, newIndex) => {
+    console.log(`slide transition from ${oldIndex} to ${newIndex}`);
+  }
+};
+
+const SliderView = () => {
+  return (
+    <div className="slide-container">
+      <Slide {...properties}>
+        <div className="each-slide">
+          <div style={{ backgroundImage: `url(${slideImages[0]})` }}>
+            <span>Slide 1</span>
+          </div>
+        </div>
+        <div className="each-slide">
+          <div style={{ backgroundImage: `url(${slideImages[1]})` }}>
+            <span>Slide 2</span>
+          </div>
+        </div>
+        <div className="each-slide">
+          <div style={{ backgroundImage: `url(${slideImages[2]})` }}>
+            <span>Slide 3</span>
+          </div>
+        </div>
+      </Slide>
+    </div>
+  );
+};
+
+export default SliderView;
